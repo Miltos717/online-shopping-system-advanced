98c5f960f1e62c573c98265676019a48063f2a87
Miltiadis Maleas <120583796+Miltos717@users.noreply.github.com>
2025-05-14 15:35:11 +0300

    Update violations.php

--- violations.php
+++ violations.php
@@ -17,12 +17,12 @@ interface TestViolations
    /**
      * ScheduledTask constructor.
      *
-     * @param string      $identifier
-     * @param string      $identifierType
-     * @param string      $taskType
-     * @param string|null $dataType
-     * @param int|null    $timestamp
-     * @param string|null $metadata
+     * @param string        $identifier
+     * @param string        $identifierType
+     * @param string        $taskType
+     * @param string|null   $dataType
+     * @param int|null      $timestamp
+     * @param string|null   $metadata
 */
    
     public function getDeletionCounter(
