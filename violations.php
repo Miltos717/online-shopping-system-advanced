d244b9eb2ff86282998f8e805d7ff9bcaf9e2ea8
Miltiadis Maleas <120583796+Miltos717@users.noreply.github.com>
2025-05-14 15:25:33 +0300

    Update violations.php

--- violations.php
+++ violations.php
@@ -11,18 +11,20 @@ use Miltos\Model\DeletionCounter;
  *
  * @package Miltos
  */
+
 interface TestViolations
 {
    /**
      * ScheduledTask constructor.
      *
-     * @param string $identifier
-     * @param string $identifierType
-     * @param string $taskType
+     * @param string      $identifier
+     * @param string      $identifierType
+     * @param string      $taskType
      * @param string|null $dataType
-     * @param int|null $timestamp
+     * @param int|null    $timestamp
      * @param string|null $metadata
 */
+   
     public function getDeletionCounter(
         string  $deletionType,
         string  $labelpartner,
