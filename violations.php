e56154187c7b21638250c81ec369030d1895a9a3
Miltiadis Maleas <120583796+Miltos717@users.noreply.github.com>
2025-05-14 14:58:54 +0300

    Update violations.php

--- violations.php
+++ violations.php
@@ -1,10 +1,33 @@
+<?php
+
+namespace Miltos;
+
+use Doctrine\ORM\Exception\NotSupported;
+use Miltos\Model\DeletionCounter;
+
 /**
-     * ScheduledTask constructor.
+ * Interface TestViolations
+ *
+ *
+ * @package Miltos
+ */
+interface TestViolations
+{
+    /**
+     * type.
      *
-     * @param string $identifier
-     * @param string $identifierType
-     * @param string $taskType
-     * @param string|null $dataType
-     * @param int|null $timestamp
-     * @param string|null $metadata
-*/
+     * @param string $deletionType
+     * @param string $labelpartner
+     * @param string|null $serviceType
+     *
+     * @return DeletionCounter|null
+     * @throws NotSupported
+     */
+    public function getDeletionCounter(
+        string  $deletionType,
+        string  $labelpartner,
+        ?string $serviceType = null
+    ): ?DeletionCounter;
+
+    public function update(DeletionCounter $deletionCounter): void;
+}
