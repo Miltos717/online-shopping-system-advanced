77010815bf4c2fb948da28ee4325efb3b93c26f5
Miltiadis Maleas <120583796+Miltos717@users.noreply.github.com>
2025-05-14 14:54:11 +0300

    Create violations.php

---
+++ violations.php
@@ -0,0 +1,10 @@
+/**
+     * ScheduledTask constructor.
+     *
+     * @param string $identifier
+     * @param string $identifierType
+     * @param string $taskType
+     * @param string|null $dataType
+     * @param int|null $timestamp
+     * @param string|null $metadata
+*/
